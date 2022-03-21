0864d9cb2895e33230bb16a4490832cedf9b19bc
Sofiane Paris <benzait.ingenieur@gmail.com>
2022-03-21 11:00:51 +0100

    first example with yargs (parsing argv)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,37 @@
 const chalk = require('chalk')
-console.log(chalk.green('Hello world!'));
+const yargs = require('yargs')
+
+
+yargs.command({
+  command  : 'add',
+  describe : 'add new note',
+  handler  : function() {
+    console.log('adding stuff..!')
+  }
+})
+
+yargs.command({
+  command  : 'remove',
+  describe : 'remove note',
+  handler  : function() {
+    console.log('removing stuff..!')
+  }
+})
+
+yargs.command({
+  command  : 'list',
+  describe : 'list elements in note',
+  handler  : function(){
+    console.log('listing elements..')
+  }
+})
+
+yargs.command({
+  command  : 'read',
+  describe : 'read elmement in note',
+  handler  : function(){
+    console.log('reading elements..')
+  }
+})
+
+yargs.parse();
