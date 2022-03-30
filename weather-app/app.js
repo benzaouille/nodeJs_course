60f3d6ccf0abaf10fb57ebffe571197b1a3c2951
Sofiane Paris <benzait.ingenieur@gmail.com>
2022-03-30 21:16:22 +0200

    chaining the callback function in weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,19 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+if(process.argv[2]){
+  geocode(process.argv[2], (error, data) => {
+    if(error){
+      return console.log(error)
+    }
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
-
-geocode('Paris', (error, data) => {
-  console.log(data)
-})
+    forecast(data.latitude, data.longitude, (error, data) => {
+      if(error){
+        return console.log(error)
+      }
+      console.log(data)
+    })
+  })
+} else{
+  console.log('there is no argument given')
+}
