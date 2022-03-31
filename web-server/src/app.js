68ec82621efaac47ae0367174b8f6be9bd59090d
Sofiane Paris <benzait.ingenieur@gmail.com>
2022-03-31 22:30:11 +0200

    adding more stuff in web page with basic balise and custom objet

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,30 @@ const Express = require('express')
 const app = Express()
 
 app.get('', (req, res) =>{
-  res.send('Hello express')
+  res.send('<h1>Hello express<\h1>')
 })
 
 app.get('/help', (req,res) =>{
-  res.send('Help page')
+  //si on envoi un objet (ou plusieurs comme dans l'exaple precedant)
+  // ils sont automatiquement converti avec stringify pour ensuite être affiché sur la page
+  res.send([{
+    name : 's2z',
+    age : 27
+  },{
+    name : 'p20',
+    age : 26
+  }])
 })
 
 app.get('/about', (req,res) =>{
-  res.send('About page')
+  res.send('<h1>About page<\h1>')
 })
 
 app.get('/weather', (req,res) =>{
-  res.send('Weather page')
+  res.send({
+    forecast : 'it is raining',
+    location : 'Paris'
+  })
 })
 
 app.listen(3000, () =>{
