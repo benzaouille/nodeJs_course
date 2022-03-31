00ae0cd04e5d5544e3fecbd44c92672784daed87
Sofiane Paris <benzait.ingenieur@gmail.com>
2022-03-31 23:06:23 +0200

    refactoring the previous commit with the use of static html pages and app.use(express.static(...))

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,17 @@
+const path = require('path')
 const Express = require('express')
 
 const app = Express()
 
-app.get('', (req, res) =>{
+//app.use(...) va reproduire exactement :
+/*app.get('', (req, res) =>{
   res.send('<h1>Hello express<\h1>')
-})
-
-app.get('/help', (req,res) =>{
-  //si on envoi un objet (ou plusieurs comme dans l'exaple precedant)
-  // ils sont automatiquement converti avec stringify pour ensuite être affiché sur la page
-  res.send([{
-    name : 's2z',
-    age : 27
-  },{
-    name : 'p20',
-    age : 26
-  }])
-})
-
-app.get('/about', (req,res) =>{
-  res.send('<h1>About page<\h1>')
-})
+})*/
+//en allant directement chercher le fichier index.html pour la racine du site web
+//donc ne jamais creer un fichier index.html pour une tout autre chose
+//pareil pour les pages static about.html et help.html
+const staticIndexPath = path.join(__dirname, '../public')
+app.use(Express.static(staticIndexPath))
 
 app.get('/weather', (req,res) =>{
   res.send({
