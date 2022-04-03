a9c6c23f419eeee34d4cb007db01182b5961d1ce
benzait <benzait.ingenieur@gmail.com>
2022-04-03 10:06:21 +0200

    switching the static html file to the dynamic ones with hbs (in views dir)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,41 @@ const Express = require('express')
 
 const app = Express()
 
-//app.use(...) va reproduire exactement :
-/*app.get('', (req, res) =>{
-  res.send('<h1>Hello express<\h1>')
-})*/
-//en allant directement chercher le fichier index.html pour la racine du site web
-//donc ne jamais creer un fichier index.html pour une tout autre chose
-//pareil pour les pages static about.html et help.html
+/*maintenant que nous avons vu comment integrer des pages static (html)
+voyons comment remplacer ses page static en page dynamic avec hbs qui est une surcouche de
+handlebars ... (pas besoin de require même si hbs à été installé avec npm)*/
+app.set('view engine', 'hbs')
+app.get('', (req,res) => {
+  //ici les données sont envoyées de l'app vers la page (dynamisme)
+  //index va directement être rechercher dans le dossier viex a la racine de projet
+  //inutile de rajouter l'extension .hbs pour index
+  res.render('index', {
+    title : 'Weather App',
+    name : 'S2z'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title : 'About page',
+    name : 'S2z'
+  })
+})
+
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title : 'Help Page',
+    helpMsg : 'how can I help you',
+    name: 'S2z'
+  })
+})
 const staticIndexPath = path.join(__dirname, '../public')
 app.use(Express.static(staticIndexPath))
+
+
+
+
 
 app.get('/weather', (req,res) =>{
   res.send({
