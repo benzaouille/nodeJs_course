d1c7cfda4527ae0524136b214150d4f206441b5e
benzait <benzait.ingenieur@gmail.com>
2022-04-03 11:23:19 +0200

    adding a header and footer with link to other page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,30 @@
 const path = require('path')
 const Express = require('express')
+const hbs = require('hbs')
 
 const app = Express()
 
+const partialFolder = path.join(__dirname, '../template/partials')
+const dynamicFolder = path.join(__dirname, '../template/views')
+const staticIndexPath = path.join(__dirname, '../public')
+app.use(Express.static(staticIndexPath))
+
+
+/*on va indiquer ou chercher les fichier partial (header) a bhs*/
+hbs.registerPartials(partialFolder)
 /*maintenant que nous avons vu comment integrer des pages static (html)
 voyons comment remplacer ses page static en page dynamic avec hbs qui est une surcouche de
 handlebars ... (pas besoin de require même si hbs à été installé avec npm)*/
 app.set('view engine', 'hbs')
+//changeons le dossier que express 'regarde' pour les fichier dynamiqye hbs (voir set dans la ref express)
+app.set('views', dynamicFolder)
+
 app.get('', (req,res) => {
   //ici les données sont envoyées de l'app vers la page (dynamisme)
   //index va directement être rechercher dans le dossier viex a la racine de projet
   //inutile de rajouter l'extension .hbs pour index
   res.render('index', {
-    title : 'Weather App',
+    title : 'Index Page',
     name : 'S2z'
   })
 })
@@ -32,12 +44,6 @@ app.get('/help', (req, res) => {
     name: 'S2z'
   })
 })
-const staticIndexPath = path.join(__dirname, '../public')
-app.use(Express.static(staticIndexPath))
-
-
-
-
 
 app.get('/weather', (req,res) =>{
   res.send({
