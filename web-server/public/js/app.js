efcb56f7b03c443787d15295def4d9b60f930942
benzait <benzait.ingenieur@gmail.com>
2022-04-05 15:27:43 +0200

    exemple of client side javascript function with fetch(..).then(..)

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
 console.log('Client side log :)')
+
+fetch('http://localhost:3000/weather?address=Paris').then((response) => {
+  response.json().then( (data) => {
+    if(data.error){
+        return console.log(data.error)
+    }
+    console.log(data)
+  })
+})
